1c5a5181f83802af412d565d2577abb811315c9f
az1k <azizillotokhirov@gmail.com>
2024-05-17 20:50:35 +0500

    finished site

---
+++ assets/js/main.js
@@ -0,0 +1,152 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+        : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+/*=============== SWIPER POPULAR ===============*/
+const popularSwiper = new Swiper('.popular__content', {
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+/*=============== CHOOSE FAQ ===============*/
+const faqItems = document.querySelectorAll('.choose__faq-item')
+
+faqItems.forEach((item) => {
+    const faqHeader = item.querySelector('.choose__faq-header')
+
+    faqHeader.addEventListener('click', () => {
+        const openItem = document.querySelector('.faq-open')
+        toggleItem(item)
+
+        if (openItem && openItem != item) {
+            toggleItem(openItem)
+        }
+    })
+})
+
+const toggleItem = (item) => {
+    const faqContent = item.querySelector('.choose__faq-content')
+
+    if (item.classList.contains('faq-open')) {
+        faqContent.removeAttribute('style')
+        item.classList.remove('faq-open')
+    } else {
+        faqContent.style.height = faqContent.scrollHeight + 'px'
+        item.classList.add('faq-open')
+    }
+
+
+}
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+    var scrollUp = document.getElementById('scroll-up');
+    if (window.scrollY >= 350) {
+        scrollUp.classList.add('show-scroll');
+    } else {
+        scrollUp.classList.remove('show-scroll');
+    }
+
+    scrollUp.addEventListener('click', function (e) {
+        e.preventDefault();
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+
+}
+window.addEventListener('scroll', scrollUp);
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const rev = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: 400
+})
+
+rev.reveal('.home__content, .popular__container, .products__container, .join__bg, .footer__container')
+rev.reveal('.home__image', { origin: 'bottom' })
+rev.reveal('.choose__image, .features__image', { origin: 'left' })
+rev.reveal('.choose__content, features__content', { origin: 'right' })
